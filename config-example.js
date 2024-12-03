7f3cd3fe4e01027c2c2c7e58fb52fb62909585f4
Chris Blake <chris@code.sc>
2024-12-02 21:06:36 -0500

    Move most important configuration options to env vars.

--- config-example.js
+++ config-example.js
@@ -1,41 +1,41 @@
 module.exports = {
-  TEST_SERVER: "http://localhost:8000",
-  PRODUCTION_SERVER: "https://app.example.com",
-  HOMEPAGE_URL: "https://example.com",
-  COUCHDB_HOST: "localhost",
-  COUCHDB_PORT: "5984",
-  COUCHDB_SERVER: "https://app.example.com/db",
-  PUBLIC_KEY: { "keydata": "somedata" },
-  SUPPORT_EMAIL: "some@example.com",
-  SUPPORT_URGENT_EMAIL: "urgent@example.com",
-  FRESHDESK_APPID: 12341234,
-  BEAMER_APPID: "myappidforbeamer",
-  LOGROCKET_APPID: "asdf/app-name",
-  TESTIMONIAL_URL: "https://testimonialdomain.example.com",
-  DESKTOP_SERIAL_SALT: 'some salt string',
-  DESKTOP_PURCHASE_URL: 'https://example.com/buy',
-  DESKTOP_PURCHASE_SUCCESS_URL: 'https://example.com/success',
-  STRIPE_PUBLIC_KEY: "pk_test_123412341234",
-  PRICE_DATA :
-    { USD :
-      { monthly :
+  TEST_SERVER: process.env.TEST_SERVER,
+  PRODUCTION_SERVER: process.env.PRODUCTION_SERVER,
+  HOMEPAGE_URL: process.env.HOMEPAGE_URL,
+  COUCHDB_HOST: process.env.COUCHDB_HOST,
+  COUCHDB_PORT: process.env.COUCHDB_PORT,
+  COUCHDB_SERVER: process.env.COUCHDB_SERVER,
+  PUBLIC_KEY: process.env.PUBLIC_KEY,
+  SUPPORT_EMAIL: process.env.SUPPORT_EMAIL,
+  SUPPORT_URGENT_EMAIL: process.env.SUPPORT_URGENT_EMAIL,
+  FRESHDESK_APPID: process.env.FRESHDESK_APPID,
+  BEAMER_APPID: process.env.BEAMER_APPID,
+  LOGROCKET_APPID: process.env.LOGROCKET_APPID,
+  TESTIMONIAL_URL: process.env.TESTIMONIAL_URL,
+  DESKTOP_SERIAL_SALT: process.env.DESKTOP_SERIAL_SALT,
+  DESKTOP_PURCHASE_URL: process.env.DESKTOP_PURCHASE_SUCCESS_URL,
+  DESKTOP_PURCHASE_SUCCESS_URL: process.env.STRIPE_PUBLIC_KEY,
+  STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+  PRICE_DATA:
+    { USD:
+      { monthly:
         { discount: "price_id_234234"
         , regular: "price_id_12341234"
         , bonus: "price_id_542543"
         }
-      , yearly :
+      , yearly:
         { discount: "price_id_1234"
         , regular: "price_id_1234"
         , bonus: "price_id_1234"
         }
       }
-    , etc :
-      { monthly :
+    , etc:
+      { monthly:
         { discount: "price_1234"
         , regular: "price_1234"
         , bonus: "price_1234"
         }
-      , yearly :
+      , yearly:
         { discount: "price_1234"
         , regular: "price_1234"
         , bonus: "price_1234"
